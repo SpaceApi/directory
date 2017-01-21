b284000c8c7d892f6d11a9bfd70c6548a9f7fab8
Danilo Bargen <mail@dbrgn.ch>
2017-01-21 23:29:52 +0100

    Refactor out `check_space` function

--- remove_unresponsive.py
+++ remove_unresponsive.py
@@ -19,22 +19,37 @@ spaces = json.loads(directory.read())
 spaces_new = {}
 has_error = False
 
+
+def check_space(url):
+    """
+    Check a space URL. Return `true` if it's OK, or `false` if it should be
+    removed.
+    """
+    try:
+        response = requests.get(url, verify=False, timeout=10)
+    except Exception as e:
+        print('  \033[0;31m-> Error: {}\033[0m'.format(e).encode('utf8'))
+        global has_error
+        has_error = True
+        return False
+    if response.status_code == 200:
+        return True
+    else:
+        print('  \033[0;31m-> Status: %s: %s\033[0m'
+                .format(response.status_code, response.reason)
+                .encode('utf8'))
+        return False
+
+
 # Check spaces
 for name, url in spaces.items():
     if 'spaceapi.net' in url:
         continue
-    try:
-        print('+ {} {}'.format(name, url).encode('utf8'))
-        response = requests.get(url, verify=False, timeout=10)
-        if response.status_code == 200:
-            spaces_new[name] = url
-        else:
-            print('  \033[0;31m-> Status: %s: %s\033[0m'
-                    .format(response.status_code, response.reason)
-                    .encode('utf8'))
-    except Exception as e:
-        print('  \033[0;31m-> Error: {}\033[0m'.format(e).encode('utf8'))
-        has_error = True
+    print('+ {} {}'.format(name, url).encode('utf8'))
+    space_valid = check_space(url)
+    if space_valid is True:
+        spaces_new[name] = url
+
 
 directory.close()
 
