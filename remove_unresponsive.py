f1adfef57602095600cbe6c5e311a8bf67c493e4
Danilo Bargen <mail@dbrgn.ch>
2017-01-21 23:19:30 +0100

    requests.get returns a response

--- remove_unresponsive.py
+++ remove_unresponsive.py
@@ -17,12 +17,12 @@ for name, url in spaces.items():
         continue
     try:
         print '%s %s' % (name, url)
-        req = requests.get(url, verify=False, timeout=10)
-        if req.status_code == 200:
+        response = requests.get(url, verify=False, timeout=10)
+        if response.status_code == 200:
             spaces_new[name] = url
         else:
             print '\033[0;31m-> Status: %s: %s\033[0m' \
-                % (req.status_code, req.reason)
+                % (response.status_code, response.reason)
     except Exception,e:
         print '\033[0;31m-> Error: %s\033[0m' % (e)
         has_error = True
