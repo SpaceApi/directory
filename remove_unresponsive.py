307fb669b9ea9ab2a60fec066afc5d5ddda21f27
Danilo Bargen <mail@dbrgn.ch>
2017-01-21 23:19:15 +0100

    Clearer error formatting

--- remove_unresponsive.py
+++ remove_unresponsive.py
@@ -21,10 +21,10 @@ for name, url in spaces.items():
         if req.status_code == 200:
             spaces_new[name] = url
         else:
-            print '\t\033[0;31m\\_ Status: %s: %s\033[0m' \
+            print '\033[0;31m-> Status: %s: %s\033[0m' \
                 % (req.status_code, req.reason)
     except Exception,e:
-        print '\t\033[0;31m\\_Error: %s\033[0m' % (e)
+        print '\033[0;31m-> Error: %s\033[0m' % (e)
         has_error = True
 
 directory.close()
